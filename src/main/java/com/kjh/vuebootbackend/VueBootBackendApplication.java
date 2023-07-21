9ebea14fdbe0e3602977a6a5a15fddce00c666df
kjy <nashirah23@gmail.com>
2023-07-21 22:45:53 +0900

    add EnableJpaAuditing

--- src/main/java/com/kjh/vuebootbackend/VueBootBackendApplication.java
+++ src/main/java/com/kjh/vuebootbackend/VueBootBackendApplication.java
@@ -2,8 +2,10 @@ package com.kjh.vuebootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VueBootBackendApplication {
 
     public static void main(String[] args) {
