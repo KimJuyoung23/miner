aa63fc9745b52b9129f28769d105b81d055b9700
qzce <speed9742@naver.com>
2023-08-05 17:44:50 +0900

    test commit

--- src/main/java/com/kjh/vuebootbackend/BoardRepository.java
+++ src/main/java/com/kjh/vuebootbackend/BoardRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    // Query return type
+    // Query return type test
     // Optional<>, List<>, Set<> 등 사용 가능
 
 //    Board findByTitle(String title); //User 복수라면 오류 발생. List로 복수 오류 해소
